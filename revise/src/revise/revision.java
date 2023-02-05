9b9d6ab7a10eb0eb80bd50386172c53ca9037e9c
gayathridevi-s <109417240+gayathridevi-s@users.noreply.github.com>
2023-02-06 00:58:36 +0530

    private access modifiers accessed using getters and setters

--- revise/src/revise/revision.java
+++ revise/src/revise/revision.java
@@ -1,18 +1,50 @@
 package revise;
 class Person{
-	String name;
-	int age;
+	private int data;
+
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+	
+//	String name;
+//	int age;
+//	void speak() {
+//		System.out.println("hello");
+//		System.out.println("my name is "+name+"\n"+"my age is"+age);
+//	}
+//	int getAge() {
+//		return age;
+//	}
+//	public String getName() {
+//		return name;
+//	}
+//	public String setName(String name) {
+//		this.name = name;
+//		return name;
+//	}
 }
 public class revision {
 public static void main(String[] args) {
-	Person person1 = new Person();
-	person1.name="Gayathri";
-	person1.age=23;
-	System.out.println(person1.age);
-	Person person2 = new Person();
-	person2.name="anju";
-	person2.age=21;
-	System.out.println(person2.name);
+	Person person1=new Person();
+	person1.setData(23);
+	System.out.println(person1.getData());
+//	Person person1 = new Person();
+//	person1.name="Gayathri";
+//	person1.age=23;
+//	person1.speak();
+//	System.out.println(person1.age);
+//	Person person2 = new Person();
+//	person2.name="anju";
+//	person2.age=21;
+//	person2.speak();
+//	person2.getAge();
+//	//person2.setName("koy");
+//	System.out.println(person2.setName("koy"));
+//	System.out.println(person2.getAge());
 	
 }
 }
